ec91f11b39d1b24dc3c359da3b2f8309497beedf
suraj h <surajj.hiremath@gmail.com>
2022-12-27 09:31:25 +0530

    styles added , list.ejs modified

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express");
 const app=express();
 const bodyParser=require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set('view engine','ejs');
 var items=[];
 
